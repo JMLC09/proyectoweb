5219d60020f9e39b16c790c12b3ce42b2afeff0a
Jose Lugo <19340481@itnogales.edu.mx>
2023-05-04 10:27:57 -0700

    Estructura del proyecto y documentos iniciales

---
+++ bdd/database.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'host' => 'localhost',
+    'username' => 'root',
+    'password' => '',
+    'database' => 'gestion_tareas',
+    'charset' => 'utf8mb4',
+];
---
+++ public/javascript/main.js
@@ -0,0 +1,8 @@
+// Funciones para mostrar/ocultar el menú de navegación
+function mostrarMenu() {
+  document.getElementById("navbar").classList.add("show");
+}
+
+function ocultarMenu() {
+  document.getElementById("navbar").classList.remove("show");
+}
